52e47a8b56e7bb71904d8ba7fee5d11bd8a5eb17
Kamil Olber <olb.kamil@gmail.com>
2018-09-29 11:45:40 +0200

    wybieranie elem z tablicy

--- arrays.js
+++ arrays.js
@@ -7,10 +7,23 @@ b[3].push("d");
 
 console.log(a,b);
 */
+const a = [1, 2, 3, 4, 5, 6];
+//##################3###########
+//cwiczenie wybieranie elementów
+/*
 
-var a = ['zero', 'one', 'two', 'three'];
-var sliced = a.slice(1,3);
 
-console.log(a); // zero, one ,two, three
+//const selectedItem = [a[1]];
+const selectedItem = a.slice(1, 2); //[2]
 
-console.log(sliced);   //one two
+const lastElement = a.slice(a.length -1);// [6]
+//cosnt lastElement = a.pop(); [6]
+
+const x = selectedItem.concat(lastElement);
+
+console.log(x);
+*/
+
+//////// rozw nr 2
+
+console.log([...a.slice(1,2), a.pop()]);
